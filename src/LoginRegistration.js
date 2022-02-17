6882351e51b9ffd8ca90a979f7f4d01ed0821f0b
FranklinTrinh7624 <78276637+FranklinTrinh7624@users.noreply.github.com>
2022-02-17 17:52:40 -0600

    updated files

--- src/LoginRegistration.js
+++ src/LoginRegistration.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.css";
 
 function LoginRegistration() {
+  const [userRegister, setUserRegister] = useState("")
+  const [passwordRegister, setPasswordRegister] = useState("")
   return (
     <div>
       <h1>loginregtex</h1>
     </div>
+
+    <div className = ""    
   );
 }
 
+
+
 export default LoginRegistration;
+
+
