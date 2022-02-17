efa6cebd3755f3c4d962de8ad877d820af39e616
FranklinTrinh7624 <78276637+FranklinTrinh7624@users.noreply.github.com>
2022-02-17 17:55:28 -0600

    updated files

--- src/LoginRegistration.js
+++ src/LoginRegistration.js
@@ -2,19 +2,13 @@ import React, { useState } from "react";
 import "./styles.css";
 
 function LoginRegistration() {
-  const [userRegister, setUserRegister] = useState("")
-  const [passwordRegister, setPasswordRegister] = useState("")
+  const [userRegister, setUserRegister] = useState("");
+  const [passwordRegister, setPasswordRegister] = useState("");
   return (
     <div>
-      <h1>loginregtex</h1>
+      <h1>loginregtexsds</h1>
     </div>
-
-    <div className = ""    
   );
 }
 
-
-
 export default LoginRegistration;
-
-
