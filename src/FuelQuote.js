1b43b6536ac6cf3ed208a15828c00b44584b678c
sakibzafar <60629023+sakibzafar@users.noreply.github.com>
2022-02-17 19:57:56 -0600

    Fuel Quote updated

--- src/FuelQuote.js
+++ src/FuelQuote.js
@@ -1,11 +1,57 @@
+import "./styles.css";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-function FuelQuote() {
+export default function App() {
+  const [date, setDate] = useState(new Date());
   return (
-    <div>
-      <h1>fuelquotea</h1>
+    <div className="App">
+      <h1>Fuel Quote Form</h1>
+
+      <Text>Gallons Requested:</Text>
+      <input
+        onKeyPress={(event) => {
+          if (!/[0-9]/.test(event.key)) {
+            event.preventDefault();
+          }
+        }}
+        placeholder="e.g 18"
+      />
+      <Text>
+        {"\n"}
+        {"\n"}
+        {"\n"}
+        Delivery Address: 15314 Riverside Dr.
+        {"\n"}
+        {"\n"}
+        {"\n"}
+      </Text>
+      <DatePicker selected={date} onChange={(date) => setDate(date)} />
+      <Text>
+        {"\n"}
+        {"\n"}
+        {"\n"}
+        Suggested Price: $29
+      </Text>
+      <Text>
+        {"\n"}
+        {"\n"}
+        {"\n"}
+        Total amount due: $30
+        {"\n"}
+        {"\n"}
+        {"\n"}
+      </Text>
     </div>
   );
 }
 
-export default FuelQuote;
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    padding: 8,
+    margin: 10
+  }
+});
