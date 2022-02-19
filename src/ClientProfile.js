00e0487c7deadeb1a0f626ad5cc322146f00805c
sakibzafar <60629023+sakibzafar@users.noreply.github.com>
2022-02-18 18:32:14 -0600

    furl quote history

--- src/ClientProfile.js
+++ src/ClientProfile.js
@@ -9,3 +9,67 @@ function ClientProfile() {
 }
 
 export default ClientProfile;
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: null,
+      lastName: null,
+      firstAddress: null,
+      secondAddress: null,
+      city: null,
+      state: null,
+      zipcode: null,
+      errors: {
+        firstName: "",
+        lastName: "",
+        firstAddress: "",
+        secondAddress: "",
+        city: "",
+        state: "",
+        zipcode: ""
+      }
+    };
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { info, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (info) {
+      case "firstName":
+        errors.firstName = value.length > 50 ? "Maximum is 50 characters" : "";
+        break;
+      case "lastName":
+        errors.lastName = value.length > 50 ? "Maximum is 50 characters" : "";
+        break;
+      case "firstAddress":
+        errors.firstAddress =
+          value.length > 100 ? "Maximum is 100 characters" : "";
+        break;
+      case "secondAddress":
+        errors.secondAddress =
+          value.length > 100 ? "Maximum is 100 characters" : "";
+        break;
+      case "city":
+        errors.city = value.length > 100 ? "Maximum is 100 characters" : "";
+        break;
+      case "state":
+        errors.state = value.length < 2 ? "Please select a state" : "";
+        break;
+      case "zipcode":
+        if (value.length < 5) {
+          errors.zipcode = "At least 5 characters required";
+        } else if (value.length > 9) {
+          errors.zipcode = "Maximum is 9 characters";
+        }
+        break;
+      default:
+        break;
+    }
+    this.setState({ errors, [info]: value }, () => console.log(this.state));
+  };
+}
--- src/FuelQuoteHistory.js
+++ src/FuelQuoteHistory.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.css";
+import data from "./mock-data.json";
 
 function FuelQuoteHistory() {
+  const [history, setHistory] = useState(data);
+
   return (
-    <div>
-      <h1>fuelhistorytex</h1>
+    <div className="FuelQuoteHisotry">
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Gallons Requested</th>
+            <th>Delivery Address</th>
+            <th>Delivery Date</th>
+            <th>Suggested Price</th>
+            <th>Total Amount Due</th>
+          </tr>
+        </thead>
+        <tbody>
+          {history.map((historySet) => (
+            <tr>
+              <td>{historySet.Date}</td>
+              <td>{historySet.GallonsRequested}</td>
+              <td>{historySet.DeliveryAddress}</td>
+              <td>{historySet.DeliveryDate}</td>
+              <td>{historySet.SuggestedPrice}</td>
+              <td>{historySet.TotalAmountDue}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
